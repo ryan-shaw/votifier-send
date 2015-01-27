3a6be0c750639f1e0422f8e2626dfd2c3191cd8a
Ryan <ryan.shaw@min.vc>
2015-01-27 11:00:36 +0000

    Votifer done

--- index.js
+++ index.js
@@ -1,17 +1,56 @@
+var ursa = require('ursa');
+var net = require('net');
 
 module.exports = {
-    check: sendData
+    check: send
 };
 
 function sendData(settings, callback){
     settings.key = settings.key.replace(/ /g, '+');
-    settings.key = wordwrap(settings.key, 64);
-    var pubKey = new Buffer('-----BEGIN PUBLIC KEY-----\n' + settings.key + '\n-----END PUBLIC KEY-----\n', 'utf8');
-    
-    var rsa = require('./lib/rsa.js')(pubKey);
-
+    settings.key = wordwrap(settings.key, 65, true);
+    var pubKey = new Buffer('-----BEGIN PUBLIC KEY-----\n' + settings.key + '\n-----END PUBLIC KEY-----\n');
     
     var build = 'VOTE\nmcerverstatus\nryanshawty\n203.0.113.1\n'+new Date().getTime()+'\n';
     
-    callback('Not implemented');
+    var len = (256-build.length)/2;
+    for(var i = 0; i < len; i++){
+    	build += '\0';
+    }
+    var buf = new Buffer(build, 'binary');
+
+    key = ursa.createPublicKey(pubKey);
+    var data = key.encrypt(build, 'binary', 'binary', ursa.RSA_PKCS1_PADDING);
+
+    var connection = net.createConnection({
+		host: settings.host,
+		port: settings.port
+	}, function(){
+		connection.write(data, 'binary', function(){
+			connection.end();
+			callback(null);
+		});
+	});
+	connection.on('error', function(e){
+		return callback('Could not connect to server');
+	});
+}
+
+function wordwrap(str, maxWidth) {
+    var newLineStr = "\n"; done = false; res = '';
+    do {                    
+        found = false;
+
+        if (!found) {
+            res += [str.slice(0, maxWidth), newLineStr].join('');
+            str = str.slice(maxWidth);
+        }
+
+        if (str.length < maxWidth){
+        	res += str;
+            done = true;
+        }
+
+    } while (!done);
+
+    return res;
 }
--- index.spec.js
+++ index.spec.js
@@ -1,14 +1,15 @@
 var settings = {
-	key: 'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAzMfEOjjHL7ivOWnEm50B5jTYWKSjA/vl7UC81tLfmWe0g6byETSNA7AZtnL7IDfpNFgu4r8BF7H/Ta6ifshKPOWxcaqvptQMmM4LJODFQZ1f5UdpViA2rwCP4M5qv0TxGAI/xu49R30RHcpKxMxct qTj4OtFsdwTHDdqHfObCCZZTSdbLDBdIq45n/kLashaTyU7foDnj7hg5rDY91ol zNNzc4g8cWI1Fp70g47IVcSqVtTjrZtJaaumaf2XCctwkTCIFvUWXRcCKbFxbOntiYP5R7CoMY/3SEVrTjKhYWfnLjv5m0lBx/Pc0KRxf3o/pWG9LBvPx29ox4AWgABQIDAQAB',
-	host: '198.50.219.246',
+	key: 'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAm5S/RAbzJ6qBma3KCaUPgkLBMPTBektUlVXxdUUUvuzbsXZ+B7+3TzADL5SMuRERfpe8qJxJxFqAtE/SX6wQlZlRodnb9oQWC7ZSaCMxCmmRf9rRIDQQRVsSS49j+ETR4/CYn4xNONgleMMvppLQfBBEJIC/UDKF8ny5HTmg09MZtLNkqya4H+s33KonwfOHI0VSfyEWpmbnzWDKioyvPXY70oexIlu6n5ma2CIadJ3JuvYjqTgdxEPPx+sT+ahjigZuY0GByrt4YGOndHnlDlE1HZpv6EnqMsD8vQvrEqFWQTSga/14u4ShQrh/pK6F9jA92K5QIXUP4Di3YNnTRQIDAQAB',
+	host: 'localhost',
 	port: 8192
 };
+settings.key = settings.key.replace(/ /g, '+');
 
 var votifier = require('./index.js');
 
 describe('votifier', function(){
     it('should send votifier data', function(done){
-        votifier.send(settings, function(err){
+        votifier.check(settings, function(err){
            if(err) return done(err); 
            done();
         });
